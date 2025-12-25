eb60372ce8d4c54333b3dd1827a21bf800f2793c
ARSukhadiya <abhisheksukhadiya3899@gmail.com>
2025-12-24 19:49:39 -0800

    Fix base path for GitHub Pages project repository deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 // If deploying to username.github.io/repo-name, use base: '/repo-name/'
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change to '/abhishek-sukhadiya/' if using a project repository
+  base: '/abhishek-sukhadiya/', // Required for project repository deployment
 })
 
